4477c11541b3db83655ec44967023c0c78b9cea7
ACCH MissionAdmin <missionadmin@ACCHs-Air.attlocal.net>
2020-07-10 21:04:30 -0500

    added ability for human to choose to set code

--- game.rb
+++ game.rb
@@ -7,30 +7,24 @@ class Game
         @code_breaker = CodeBreaker.new
     end
 
-    def start_new_game
-        puts 'Welcome to Mastermind! What is your name?'
-        name = gets.chomp
-        puts "Greetings #{name.capitalize!}! A randomly selected secret code is being generated."
-        secret_code = @code_maker.generate_secret_code
-    end
-
     def receive_guess
-            puts "Please enter four colors(separated by a comma)[enter 'o' for options]: "
-            guess = gets.chomp
-            if guess == 'o'
-                puts "Your options are red, blue, yellow, green, purple or maroon."
-                receive_guess
-            else
-                # turn user guess into an array
-                user_array = guess.split(", ")
-                # map user array to match style of code_maker array
-                formatted_guess = user_array.map { |element| element.capitalize}
-                formatted_guess
-            end
+        puts "Please enter four colors(separated by a comma)[enter 'o' for options]: "
+        guess = gets.chomp
+        if guess == 'o'
+            puts "Your options are red, blue, yellow, green, purple or maroon."
+            receive_guess
+        else
+            # turn user guess into an array
+            user_array = guess.split(", ")
+            # map user array to match style of code_maker array
+            formatted_guess = user_array.map { |element| element.capitalize}
+            formatted_guess
+        end
     end
 
-    def compare_guess_to_secret_code
-        secret_code = start_new_game
+    def computer_is_mastermind
+        puts "A randomly selected secret code is being generated."
+        secret_code = @code_maker.generate_secret_code
         guess = receive_guess
         response = @code_maker.compare_guess_to_secret_code(guess)
         until response == "You did it!" || @code_breaker.game_over?
@@ -44,7 +38,45 @@ class Game
             puts "You did it! The secret code was #{secret_code}"
         end
     end
+
+    def start_new_game
+        puts 'Welcome to Mastermind! What is your name?'
+        name = gets.chomp
+        puts "Greetings #{name.capitalize!}! Would you like to be the 'Mastermind' or the 'Decoder'?"
+        user = gets.chomp
+        if user.capitalize == 'Mastermind'
+            user_is_mastermind
+        elsif user.capitalize == 'Decoder'
+            computer_is_mastermind
+        else
+            puts "I don't understand. You'll have to try again!"
+        end
+    end
+
+    def user_creates_code
+        # user creates code
+        puts "Please enter four colors separated by a comma(choices are red, blue, green, purple, yellow, maroon):"
+        user_input = gets.chomp
+        secret_code = user_input.split(", ").map { |element| element.capitalize }
+        # if the secret code includes something other than the original colors, throw an exception
+        p secret_code
+    end
+
+    def computer_guess(secret_code)
+        # computer guesses code
+        computer_guess = @code_maker.generate_secret_code
+        p "My guess: #{computer_guess}"
+        until computer_guess == secret_code || @code_breaker.game_over?
+            @code_breaker.used_a_turn
+            computer_guess
+        end
+    end
+
+    def user_is_mastermind
+        secret_code = user_creates_code
+        computer_guess(secret_code)
+    end
 end
 
 new_game = Game.new
-new_game.compare_guess_to_secret_code
+new_game.start_new_game
